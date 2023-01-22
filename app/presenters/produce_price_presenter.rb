764f391e9c762dcb1dec1ca0cecda2f9cb981266
Chris Li <chris.lty07@gmail.com>
2023-01-21 22:19:17 -0500

    implement timeline of single produce item

--- app/presenters/produce_price_presenter.rb
+++ app/presenters/produce_price_presenter.rb
@@ -3,11 +3,12 @@ class ProducePricePresenter
   DATE = 'date'
   LABEL = 'label'
   PRICE = 'price'
+  UNIT = 'unit'
 
   def initialize(input, items: nil)
     @produce_prices = input
+    @items = items
     @response = {}
-    @response[:items] = items
     @response[:prices] = []
   end
 
@@ -18,12 +19,32 @@ class ProducePricePresenter
   private
 
   def timeline
-    @produce_prices.all.each do |row| 
+    @items.each do |item|
+      prices = []
+      dates = []
+      units = []
       record = {}
-      record[DATE] = row[DATE]
-      record[row[LABEL]] = row[PRICE].to_f
+
+      @produce_prices.all.where(label: item).each do |row|
+        prices.append(row[PRICE])
+        dates.append(row[DATE])
+        units.append(unit_label(row[UNIT]))
+      end
+
+      record[LABEL] = item
+      record[DATE+'s'] = dates
+      record[PRICE+'s'] = prices
+      record[UNIT+'s'] = units
       @response[:prices].append(record)
     end
     @response
+  end
+
+  def unit_label(row_unit_label)
+    if row_unit_label == "each"
+      "each"
+    elsif row_unit_label == "per pound"
+      "lb"
+    end
   end
 end
