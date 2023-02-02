c8937f87d5bbe0f3e4b20c3d67c47f0233621ef4
brian <bsipin@gmail.com>
2023-02-02 15:09:18 -0500

    trying client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
 
   const request = async () => {
-    let req = await fetch(`/producepricetimeline?search=${search}`);
+    let req = await fetch(`https://parkslopeproduce-api-v2.onrender.com/producepricetimeline?search=${search}`);
     let res = await req.json();
     if (req.ok) {
       setPriceData(res.prices);
