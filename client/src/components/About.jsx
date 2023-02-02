339c71cea3b6e2916e2994b546933db3f40d7c67
Chris Li <chris.lty07@gmail.com>
2023-02-02 17:25:14 -0500

    Typo in About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return (
         <div>
             <h3>Welcome to Parkslope Coop!</h3>
-            <p>This app tracks the historical produce prices ...</p>
+            <p>This app tracks historical produce prices ...</p>
         </div>
     )
 }
