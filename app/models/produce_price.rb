bee1ffd075cb3250b75b1c7f290d6f64fa4090f0
brian <bsipin@gmail.com>
2023-02-04 13:30:01 -0500

    fix lint

--- app/models/produce_price.rb
+++ app/models/produce_price.rb
@@ -11,9 +11,11 @@
 #  updated_at :datetime         not null
 #
 class ProducePrice < ApplicationRecord
+
   def self.update_or_create(attributes)
     assign_or_new(attributes).save
   end
+
   def self.assign_or_new(attributes)
     obj = first || new
     obj.assign_attributes(attributes)
